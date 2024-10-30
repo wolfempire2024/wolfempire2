513e4f243c1ac1c4341674b004ac862fcd0f1305
wolfempire2024 <wolfempire2024@gmail.com>
2024-10-30 16:18:03 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+const token = process.env.token;
+const prefix = config.prefix;
+
+
+client.on("ready",() => {
+
+          console.log(`Bot: [ ${client.user.username} ]\nStatus: [ ONLINE ] `)
+})
+
+
+client.on("message",async wolf => {
+
+  if(wolf.content.startsWith(prefix + "say")){
+
+    if(!wolf.member.permissions.has("ADMINISTRATOR")) return;
+    
+    let msg = wolf.content.split(' ').slice(1).join(" ")
+    wolf.delete()
+
+    let embed = new Discord.MessageEmbed()
+    .setTitle("**WoLF Empire**")
+    .setThumbnail(wolf.guild.iconURL({dynamic:true}))
+    .setColor("ORANGE")
+    .setDescription(msg)
+
+
+    wolf.channel.send(embed)
+                      
+  }
+})
+
+client.login(token)
